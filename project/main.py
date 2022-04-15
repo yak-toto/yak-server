4df3149c15e199df7010394945a25cd7060265cd
clarisse <gui.lepape25@gmail.com>
2022-04-16 01:56:56 +0200

    Add group rest call

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from .models import Match
 
 main = Blueprint('main', __name__)
 
@@ -11,4 +12,15 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
+    return render_template('profile.html', name=current_user.name)
+
+GROUPS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
+
+@main.route('/group/<group_name>')
+def group(group_name):
+    if group_name not in GROUPS:
+        return 'bad request!', 404
+
+    matches = Match.query.filter_by(name=current_user.name, group_name=group_name)
+
     return render_template('profile.html', name=current_user.name)
--- project/models.py
+++ project/models.py
@@ -6,3 +6,12 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    group_name = db.Column(db.String(1))
+    team1 = db.Column(db.String(100))
+    score1 = db.Column(db.Integer)
+    score2 = db.Column(db.Integer)
+    team2 = db.Column(db.String(100))
