ed6e6864864125eb4dc20381972ce4711aa9a74c
clarisse <gui.lepape25@gmail.com>
2022-04-19 00:20:10 +0200

    Update user table with results

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from . import db
 from .models import Match
@@ -22,5 +22,24 @@ def group(group_name):
         return 'bad request!', 404
 
     matches = list(Match.query.filter_by(name=current_user.name, group_name=group_name))
+
+    return render_template('group.html', group_name=group_name, matches=matches)
+
+@main.route('/group/<group_name>', methods=['POST'])
+def group_post(group_name):
+    if group_name not in GROUPS:
+        return 'bad request!', 404
+
+    score_first_column = request.form.getlist('score_first_column[]')
+    score_second_column = request.form.getlist('score_second_column[]')
+
+    matches = Match.query.filter_by(name=current_user.name, group_name=group_name)
+
+    for index, match in enumerate(matches):
+        match.score1 = score_first_column[index]
+        match.score2 = score_second_column[index]
+
+    (db.session.add(match) for match in matches)
+    db.session.commit()
 
     return render_template('group.html', group_name=group_name, matches=matches)
