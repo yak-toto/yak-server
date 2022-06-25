4a2cb77f4b0f324958372dcd75b66986e672cbd7
clarisse <gui.lepape25@gmail.com>
2022-06-25 14:09:14 +0200

    Delete SQL alchemy echo config

--- server/config.py
+++ server/config.py
@@ -4,7 +4,6 @@ SECRET_KEY = "d9e886ca-3de1-4382-8a0d-2450f6338fb7"
 # Create in-memory database
 DATABASE_FILE = "db.sqlite"
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_FILE
-SQLALCHEMY_ECHO = True
 
 # SQL Alchemy features
 SQLALCHEMY_TRACK_MODIFICATIONS = False
