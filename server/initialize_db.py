d1ace02a1b9f9d9427c3cbb54b2b20f817e96b4e
clarisse <gui.lepape25@gmail.com>
2022-06-30 00:23:07 +0200

    Add python script to initialize the sql database

---
+++ server/initialize_db.py
@@ -0,0 +1,23 @@
+import json
+import sqlite3
+import uuid
+
+con = sqlite3.connect("db.sqlite")
+cur = con.cursor()
+
+with open("matches.json") as file:
+    matches = json.loads(file.read())
+
+for match in matches:
+    cur.execute(
+        f"""INSERT INTO matches VALUES (
+            '{str(uuid.uuid4())}',
+            '{match["group_name"]}',
+            '{match["teams"][0]}',
+            '{match["teams"][1]}'
+        )"""
+    )
+
+con.commit()
+
+con.close()
