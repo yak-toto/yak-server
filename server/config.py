655929405b4042dbd010c1943364a8b22f6bcff8
clarisse <gui.lepape25@gmail.com>
2022-08-13 20:47:53 +0200

    Load jwt secret key from credentials.json

--- server/config.py
+++ server/config.py
@@ -1,17 +1,17 @@
 import json
 
-# Create dummy secrey key so we can use sessions
-SECRET_KEY = "d9e886ca-3de1-4382-8a0d-2450f6338fb7"
-
 # Do not sort key in json response
 JSON_SORT_KEYS = False
 
-# Create in-memory database
+# Read secret infos
 with open("credentials.json") as file:
     config = json.loads(file.read())
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{config['user']}:{config['password']}@localhost:3306/yak_toto"
     )
+
+    # Load jwt secret key from credentials file
+    SECRET_KEY = config["secret_key"]
 
 # SQL Alchemy features
 SQLALCHEMY_TRACK_MODIFICATIONS = False
