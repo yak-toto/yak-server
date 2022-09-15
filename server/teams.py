6db080c6ce223f164cf4b9eb8375934b690ee3af
clarisse <gui.lepape25@gmail.com>
2022-09-15 23:24:51 +0200

    Implement POST /teams to create a team or a collection of teams

--- server/teams.py
+++ server/teams.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
+from flask import request
 
+from . import db
 from .models import Team
 from .utils.constants import GLOBAL_ENDPOINT
 from .utils.constants import VERSION
@@ -11,6 +13,26 @@ from .utils.flask_utils import is_uuid4
 from .utils.flask_utils import success_response
 
 teams = Blueprint("team", __name__)
+
+
+@teams.route(f"/{GLOBAL_ENDPOINT}/{VERSION}/teams", methods=["POST"])
+def teams_post():
+    body = request.get_json()
+
+    if isinstance(body, list):
+        teams = [Team(**team) for team in body]
+
+        db.session.add_all(teams)
+        db.session.commit()
+
+        return success_response(201, [team.to_dict() for team in teams])
+    else:
+        team = Team(**body)
+
+        db.session.add(team)
+        db.session.commit()
+
+        return success_response(201, team.to_dict())
 
 
 @teams.route(f"/{GLOBAL_ENDPOINT}/{VERSION}/teams")
