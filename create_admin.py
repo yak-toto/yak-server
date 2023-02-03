417b7b6a4ae0aa0597da02f807492567a11815b8
clarisse <gui.lepape25@gmail.com>
2023-02-03 01:33:52 +0100

    Correct import issue in create_admin.py and improve the script

--- create_admin.py
+++ create_admin.py
@@ -1,19 +1,35 @@
+#!/usr/bin/env python
 from getpass import getpass
 
 from server import create_app
 from server import db
-from server.models import UserModel
+from server.database.models import MatchModel
+from server.database.models import ScoreBetModel
+from server.database.models import UserModel
 
 
 def script(app):
     with app.app_context():
-        db.session.add(
-            UserModel(
-                name="admin",
-                first_name="admin",
-                last_name="admin",
-                password=getpass(prompt="Admin user password: "),
-            )
+        password = getpass(prompt="Admin user password: ")
+        confirm_password = getpass(prompt="Confirm admin password: ")
+
+        if password != confirm_password:
+            print("ERROR : Password and Confirm Password fields does not match.")
+            return
+
+        user = UserModel(
+            name="admin",
+            first_name="admin",
+            last_name="admin",
+            password=password,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        db.session.add_all(
+            ScoreBetModel(user_id=user.id, match_id=match.id)
+            for match in MatchModel.query.all()
         )
         db.session.commit()
 
