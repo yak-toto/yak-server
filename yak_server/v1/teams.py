b6aebf3ad806a3d7f6c1fc026df8a6189dd515f4
clarisse <gui.lepape25@gmail.com>
2023-02-20 12:53:42 +0100

    Remove POST /teams call

--- yak_server/v1/teams.py
+++ yak_server/v1/teams.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, request
+from flask import Blueprint
 
-from yak_server import db
 from yak_server.database.models import TeamModel
 
 from .utils.constants import GLOBAL_ENDPOINT, VERSION
@@ -8,26 +7,6 @@ from .utils.errors import InvalidTeamId, TeamNotFound
 from .utils.flask_utils import is_iso_3166_1_alpha_2_code, is_uuid4, success_response
 
 teams = Blueprint("team", __name__)
-
-
-@teams.post(f"/{GLOBAL_ENDPOINT}/{VERSION}/teams")
-def teams_post():
-    body = request.get_json()
-
-    if isinstance(body, list):
-        teams = [TeamModel(**team) for team in body]
-
-        db.session.add_all(teams)
-        db.session.commit()
-
-        return success_response(201, {"teams": [team.to_dict() for team in teams]})
-    else:
-        team = TeamModel(**body)
-
-        db.session.add(team)
-        db.session.commit()
-
-        return success_response(201, {"team": team.to_dict()})
 
 
 @teams.get(f"/{GLOBAL_ENDPOINT}/{VERSION}/teams")
