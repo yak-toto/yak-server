59d0f0a0afb1bb4d7741d52a14a9f5c9ed6649fd
glepape <gui.lepape25@gmail.com>
2023-02-23 15:44:11 +0100

    Stop dropping all tables at the end of every test. Instead just delete the records.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,19 @@ import os
 import pytest
 
 from yak_server import create_app, db
+from yak_server.database.models import (
+    BinaryBetModel,
+    GroupModel,
+    MatchModel,
+    PhaseModel,
+    ScoreBetModel,
+    TeamModel,
+    UserModel,
+)
 
 
-@pytest.fixture(scope="module")
-def app():
+@pytest.fixture(scope="session")
+def app_session():
     # Override MYSQL_DB environment to push data to a different database
     os.environ["MYSQL_DB"] = "yak_toto_test"
 
@@ -28,6 +37,33 @@ def app():
     # Clean database after running test
     with app.app_context():
         db.drop_all()
+
+
+@pytest.fixture(scope="module")
+def app(app_session):
+    # Clean database before running test
+    with app_session.app_context():
+        ScoreBetModel.query.delete()
+        BinaryBetModel.query.delete()
+        UserModel.query.delete()
+        MatchModel.query.delete()
+        GroupModel.query.delete()
+        PhaseModel.query.delete()
+        TeamModel.query.delete()
+        db.session.commit()
+
+    yield app_session
+
+    # Clean database after running test
+    with app_session.app_context():
+        ScoreBetModel.query.delete()
+        BinaryBetModel.query.delete()
+        UserModel.query.delete()
+        MatchModel.query.delete()
+        GroupModel.query.delete()
+        PhaseModel.query.delete()
+        TeamModel.query.delete()
+        db.session.commit()
 
 
 @pytest.fixture(scope="module")
