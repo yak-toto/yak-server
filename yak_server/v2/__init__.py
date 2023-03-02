ea02341c2411ccc3f20b0805582dfe77eb8b6a41
clarisse <gui.lepape25@gmail.com>
2023-03-02 21:09:01 +0100

    Add MaskErrors extension to strawberry schema

--- yak_server/v2/__init__.py
+++ yak_server/v2/__init__.py
@@ -1,6 +1,17 @@
 import strawberry
+from flask import current_app
+from strawberry.extensions import MaskErrors
 
 from .mutation import Mutation
 from .query import Query
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+def mask_errors(_) -> bool:
+    return not current_app.config["DEBUG"]
+
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+    extensions=[MaskErrors(should_mask_error=mask_errors)],
+)
