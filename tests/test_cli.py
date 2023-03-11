3d404d20cd110f90a19436a47acd55e2854b581e
clarisse <gui.lepape25@gmail.com>
2023-03-11 14:59:33 +0100

    Add test for backup command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,18 @@
+from datetime import datetime, timedelta
 from http import HTTPStatus
+from importlib import resources
 from unittest.mock import Mock
 
 import pytest
+from dateutil import parser
 
 from yak_server.cli.database import (
+    BackupError,
     ConfirmPasswordDoesNotMatch,
     RecordDeletionInProduction,
     SignupError,
     TableDropInProduction,
+    backup_database,
     create_admin,
     delete_database,
     drop_database,
@@ -73,3 +78,24 @@ def test_drop_all_tables(app):
         drop_database(app)
 
     app.config["DEBUG"] = True
+
+
+def test_backup(app):
+    backup_database(app)
+
+    list_datetime_backup = sorted(
+        parser.parse(str(file).split("_")[-1].split(".")[0])
+        for file in (resources.files("yak_server") / "cli/backup_files").iterdir()
+    )
+
+    # Check that most recent backup has been done 1 second ago
+    assert datetime.now() - list_datetime_backup[-1] <= timedelta(seconds=1)
+
+    # Check BackupError if password is incorrect
+    old_password = app.config["MYSQL_PASSWORD"]
+    app.config["MYSQL_PASSWORD"] = get_random_string(6)
+
+    with pytest.raises(BackupError):
+        backup_database(app)
+
+    app.config["MYSQL_PASSWORD"] = old_password
