50cc429b86bd34f42fbd354cfaddd38858fa8255
clarisse <gui.lepape25@gmail.com>
2023-03-17 19:51:36 +0100

    Add tests for yak cli

---
+++ tests/test_yak_cli.py
@@ -0,0 +1,80 @@
+import subprocess
+from http import HTTPStatus
+
+import pexpect
+
+from .test_utils import get_random_string
+
+
+def test_cli(client):
+    # Check database drop
+    result = subprocess.run(
+        "yak db drop",
+        shell=True,
+        capture_output=True,
+    )
+
+    assert result.returncode == 0
+
+    # Check database creation
+    result = subprocess.run(
+        "yak db create",
+        shell=True,
+        capture_output=True,
+    )
+
+    assert result.returncode == 0
+
+    # Check database initialization
+    result = subprocess.run(
+        "yak db init",
+        shell=True,
+        capture_output=True,
+    )
+
+    assert result.returncode == 0
+
+    # Check admin accoumt creation
+    admin_password = get_random_string(7)
+
+    child = pexpect.spawn("yak db admin")
+
+    child.expect("Admin user password: ", timeout=100)
+    child.sendline(f"{admin_password}\n")
+    child.expect("Confirm admin password: ", timeout=100)
+    child.sendline(f"{admin_password}\n")
+    child.read()
+
+    response_login = client.post(
+        "/api/v1/users/login",
+        json={
+            "name": "admin",
+            "password": admin_password,
+        },
+    )
+
+    assert response_login.status_code == HTTPStatus.CREATED
+
+    auth_token = response_login.json["result"]["token"]
+
+    # Check records deletion
+    result = subprocess.run(
+        "yak db delete",
+        shell=True,
+        capture_output=True,
+    )
+
+    assert result.returncode == 0
+
+    # Check user cannot access after records are cleaned
+    response_login_user_not_found = client.get(
+        "/api/v1/bets",
+        headers={"Authorization": f"Bearer {auth_token}"},
+    )
+
+    assert response_login_user_not_found.status_code == HTTPStatus.NOT_FOUND
+    assert response_login_user_not_found.json == {
+        "ok": False,
+        "error_code": HTTPStatus.NOT_FOUND,
+        "description": "User not found",
+    }
