544fd24215b4c1c35026d95370d67c3b2316baea
clarisse <gui.lepape25@gmail.com>
2023-03-31 20:45:14 +0200

    Implement fully customizable rule

---
+++ yak_server/helpers/rules.py
@@ -0,0 +1,112 @@
+from itertools import chain
+from operator import attrgetter
+from uuid import uuid4
+
+from sqlalchemy import and_
+
+from yak_server import db
+from yak_server.database.models import BinaryBetModel, GroupModel, MatchModel, PhaseModel
+from yak_server.v1.bets import get_group_rank_with_code
+
+
+def compute_finale_phase_from_group_rank(user, rule_config):
+    groups_result = {
+        group.code: get_group_rank_with_code(user, group.code)["group_rank"]
+        for group in GroupModel.query.join(GroupModel.phase).filter(
+            PhaseModel.code == "GROUP",
+        )
+    }
+
+    first_phase_phase_group = GroupModel.query.filter_by(
+        code=rule_config["first_group"],
+    ).first()
+
+    existing_binary_bets = BinaryBetModel.query.join(BinaryBetModel.match).filter(
+        and_(
+            BinaryBetModel.user_id == user.id,
+            MatchModel.group_id == first_phase_phase_group.id,
+        ),
+    )
+
+    new_binary_bets = []
+    new_matches = []
+
+    for index, match_config in enumerate(rule_config["versus"], 1):
+        if all(
+            team["played"] == len(groups_result[match_config["team1"]["group"]]) - 1
+            for team in chain(
+                groups_result[match_config["team1"]["group"]],
+                groups_result[match_config["team2"]["group"]],
+            )
+        ):
+            team1 = groups_result[match_config["team1"]["group"]][
+                match_config["team1"]["rank"] - 1
+            ]["team"]
+            team2 = groups_result[match_config["team2"]["group"]][
+                match_config["team2"]["rank"] - 1
+            ]["team"]
+
+            match = MatchModel.query.filter_by(
+                group_id=first_phase_phase_group.id,
+                team1_id=team1["id"],
+                team2_id=team2["id"],
+                index=index,
+            ).first()
+
+            if not match:
+                match = MatchModel(
+                    id=str(uuid4()),
+                    group_id=first_phase_phase_group.id,
+                    team1_id=team1["id"],
+                    team2_id=team2["id"],
+                    index=index,
+                )
+
+            new_matches.append(match)
+
+            bet = BinaryBetModel.query.filter_by(
+                user_id=user.id,
+                match_id=match.id,
+            ).first()
+
+            if not bet:
+                bet = BinaryBetModel(
+                    user_id=user.id,
+                    match_id=match.id,
+                )
+
+            new_binary_bets.append(bet)
+
+    # Compare existing matches/bets and new matches/bets
+    db.session.add_all(new_matches)
+    db.session.flush()
+
+    db.session.add_all(new_binary_bets)
+    db.session.flush()
+
+    is_bet_modified = False
+
+    for bet in existing_binary_bets:
+        if bet.id not in map(attrgetter("id"), new_binary_bets):
+            is_bet_modified = True
+            db.session.delete(bet)
+
+    db.session.flush()
+
+    if is_bet_modified:
+        for bet in BinaryBetModel.query.filter_by(user_id=user.id):
+            if bet.match.group.code in GroupModel.query.join(GroupModel.phase).filter(
+                and_(
+                    PhaseModel.code == "FINAL",
+                    GroupModel.id == first_phase_phase_group.id,
+                ),
+            ):
+                db.session.delete(bet)
+        db.session.flush()
+
+    db.session.commit()
+
+
+RULE_MAPPING = {
+    "492345de-8d4a-45b6-8b94-d219f2b0c3e9": compute_finale_phase_from_group_rank,
+}
--- yak_server/v1/bets.py
+++ yak_server/v1/bets.py
@@ -1,10 +1,9 @@
 import logging
 from http import HTTPStatus
-from itertools import chain
 from operator import attrgetter
 from uuid import uuid4
 
-from flask import Blueprint, current_app, request
+from flask import Blueprint, request
 from sqlalchemy import and_
 
 from yak_server import db
@@ -51,12 +50,10 @@ def create_bet(current_user, phase_code):
 
     phase = PhaseModel.query.filter_by(code=phase_code).first()
 
-    finale_phase_config = current_app.config["FINALE_PHASE_CONFIG"]
-
     groups = GroupModel.query.filter(
         and_(
             GroupModel.phase_id == phase.id,
-            GroupModel.code != finale_phase_config["first_group"],
+            GroupModel.code != "8",
         ),
     )
 
@@ -250,117 +247,3 @@ def get_group_rank_with_code(user, group_code):
     db.session.commit()
 
     return send_response(group, group_rank)
-
-
-@bets.post(f"/{GLOBAL_ENDPOINT}/{VERSION}/bets/finale_phase")
-@is_authentificated
-def commit_finale_phase(current_user):
-    finale_phase_config = current_app.config["FINALE_PHASE_CONFIG"]
-
-    groups_result = {
-        group.code: get_group_rank_with_code(current_user, group.code)["group_rank"]
-        for group in GroupModel.query.join(GroupModel.phase).filter(
-            PhaseModel.code == "GROUP",
-        )
-    }
-
-    first_phase_phase_group = GroupModel.query.filter_by(
-        code=finale_phase_config["first_group"],
-    ).first()
-
-    existing_binary_bets = BinaryBetModel.query.join(BinaryBetModel.match).filter(
-        and_(
-            BinaryBetModel.user_id == current_user.id,
-            MatchModel.group_id == first_phase_phase_group.id,
-        ),
-    )
-
-    new_binary_bets = []
-    new_matches = []
-
-    for index, match_config in enumerate(finale_phase_config["versus"], 1):
-        if all(
-            team["played"] == len(groups_result[match_config["team1"]["group"]]) - 1
-            for team in chain(
-                groups_result[match_config["team1"]["group"]],
-                groups_result[match_config["team2"]["group"]],
-            )
-        ):
-            team1 = groups_result[match_config["team1"]["group"]][
-                match_config["team1"]["rank"] - 1
-            ]["team"]
-            team2 = groups_result[match_config["team2"]["group"]][
-                match_config["team2"]["rank"] - 1
-            ]["team"]
-
-            match = MatchModel.query.filter_by(
-                group_id=first_phase_phase_group.id,
-                team1_id=team1["id"],
-                team2_id=team2["id"],
-                index=index,
-            ).first()
-
-            if not match:
-                match = MatchModel(
-                    id=str(uuid4()),
-                    group_id=first_phase_phase_group.id,
-                    team1_id=team1["id"],
-                    team2_id=team2["id"],
-                    index=index,
-                )
-
-            new_matches.append(match)
-
-            bet = BinaryBetModel.query.filter_by(
-                user_id=current_user.id,
-                match_id=match.id,
-            ).first()
-
-            if not bet:
-                bet = BinaryBetModel(
-                    user_id=current_user.id,
-                    match_id=match.id,
-                )
-
-            new_binary_bets.append(bet)
-
-    # Compare existing matches/bets and new matches/bets
-    db.session.add_all(new_matches)
-    db.session.flush()
-
-    db.session.add_all(new_binary_bets)
-    db.session.flush()
-
-    is_bet_modified = False
-
-    for bet in existing_binary_bets:
-        if bet.id not in map(attrgetter("id"), new_binary_bets):
-            is_bet_modified = True
-            db.session.delete(bet)
-
-    db.session.flush()
-
-    if is_bet_modified:
-        for bet in BinaryBetModel.query.filter_by(user_id=current_user.id):
-            if bet.match.group.code in GroupModel.query.join(GroupModel.phase).filter(
-                and_(
-                    PhaseModel.code == "FINAL",
-                    GroupModel.id == first_phase_phase_group.id,
-                ),
-            ):
-                db.session.delete(bet)
-        db.session.flush()
-
-    db.session.commit()
-
-    phase, groups, score_bets, binary_bets = bets_from_phase_code(current_user, "FINAL")
-
-    return success_response(
-        HTTPStatus.OK,
-        {
-            "phase": phase.to_dict(),
-            "groups": [group.to_dict_without_phase() for group in groups],
-            "score_bets": [score_bet.to_dict_with_group_id() for score_bet in score_bets],
-            "binary_bets": [binary_bet.to_dict_with_group_id() for binary_bet in binary_bets],
-        },
-    )
