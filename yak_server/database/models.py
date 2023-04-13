64d9c05a89c9a71cdfa0146f4ef72edcc70acfcf
clarisse <gui.lepape25@gmail.com>
2023-04-13 20:12:34 +0200

    Refactoring in sqlalchemy

--- yak_server/database/models.py
+++ yak_server/database/models.py
@@ -2,10 +2,12 @@ from datetime import datetime, timezone
 from enum import Enum
 from uuid import uuid4
 
+import sqlalchemy as sa
 from dateutil import parser
 from flask import current_app, url_for
 from sqlalchemy import CheckConstraint
 from sqlalchemy import Enum as SqlEnum
+from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from yak_server import db
@@ -13,73 +15,73 @@ from yak_server import db
 
 class UserModel(db.Model):
     __tablename__ = "user"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    first_name = db.Column(db.String(100), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    number_match_guess = db.Column(
-        db.Integer,
+    name = sa.Column(sa.String(100), unique=True, nullable=False)
+    first_name = sa.Column(sa.String(100), nullable=False)
+    last_name = sa.Column(sa.String(100), nullable=False)
+    password = sa.Column(sa.String(100), nullable=False)
+    number_match_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_match_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_score_guess = db.Column(
-        db.Integer,
+    number_score_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_score_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_qualified_teams_guess = db.Column(
-        db.Integer,
+    number_qualified_teams_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_qualified_teams_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_first_qualified_guess = db.Column(
-        db.Integer,
+    number_first_qualified_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_first_qualified_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_quarter_final_guess = db.Column(
-        db.Integer,
+    number_quarter_final_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_quarter_final_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_semi_final_guess = db.Column(
-        db.Integer,
+    number_semi_final_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_semi_final_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_final_guess = db.Column(
-        db.Integer,
+    number_final_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_final_guess>=0"),
         nullable=False,
         default=0,
     )
-    number_winner_guess = db.Column(
-        db.Integer,
+    number_winner_guess = sa.Column(
+        sa.Integer,
         CheckConstraint("number_winner_guess>=0"),
         nullable=False,
         default=0,
     )
 
-    points = db.Column(
-        db.Float,
+    points = sa.Column(
+        sa.Float,
         CheckConstraint("points>=0"),
         nullable=False,
         default=0,
     )
 
-    score_bets = db.relationship(
+    score_bets = relationship(
         "ScoreBetModel",
         back_populates="user",
         lazy="dynamic",
@@ -87,7 +89,7 @@ class UserModel(db.Model):
         passive_deletes=True,
     )
 
-    binary_bets = db.relationship(
+    binary_bets = relationship(
         "BinaryBetModel",
         back_populates="user",
         lazy="dynamic",
@@ -142,24 +144,24 @@ def is_locked(user_name) -> bool:
 
 class ScoreBetModel(db.Model):
     __tablename__ = "score_bet"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    user_id = db.Column(
-        db.String(100),
-        db.ForeignKey("user.id", ondelete="CASCADE"),
+    user_id = sa.Column(
+        sa.String(100),
+        sa.ForeignKey("user.id", ondelete="CASCADE"),
         nullable=False,
     )
-    user = db.relationship("UserModel", back_populates="score_bets")
+    user = relationship("UserModel", back_populates="score_bets")
 
-    match_id = db.Column(db.String(100), db.ForeignKey("match.id"), nullable=False)
-    match = db.relationship("MatchModel", back_populates="score_bets")
+    match_id = sa.Column(sa.String(100), sa.ForeignKey("match.id"), nullable=False)
+    match = relationship("MatchModel", back_populates="score_bets")
 
-    score1 = db.Column(db.Integer, CheckConstraint("score1>=0"), default=None)
-    score2 = db.Column(db.Integer, CheckConstraint("score2>=0"), default=None)
+    score1 = sa.Column(sa.Integer, CheckConstraint("score1>=0"), default=None)
+    score2 = sa.Column(sa.Integer, CheckConstraint("score2>=0"), default=None)
 
     def is_invalid(self) -> bool:
         return None in (self.score1, self.score2)
@@ -213,23 +215,23 @@ class ScoreBetModel(db.Model):
 
 class BinaryBetModel(db.Model):
     __tablename__ = "binary_bet"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    user_id = db.Column(
-        db.String(100),
-        db.ForeignKey("user.id", ondelete="CASCADE"),
+    user_id = sa.Column(
+        sa.String(100),
+        sa.ForeignKey("user.id", ondelete="CASCADE"),
         nullable=False,
     )
-    user = db.relationship("UserModel", back_populates="binary_bets")
+    user = relationship("UserModel", back_populates="binary_bets")
 
-    match_id = db.Column(db.String(100), db.ForeignKey("match.id"), nullable=False)
-    match = db.relationship("MatchModel", back_populates="binary_bets")
+    match_id = sa.Column(sa.String(100), sa.ForeignKey("match.id"), nullable=False)
+    match = relationship("MatchModel", back_populates="binary_bets")
 
-    is_one_won = db.Column(db.Boolean, default=None)
+    is_one_won = sa.Column(sa.Boolean, default=None)
 
     def bet_from_is_one_won(self) -> tuple:
         if self.is_one_won is None:
@@ -290,58 +292,58 @@ class BetMapping(Enum):
 
 class MatchReferenceModel(db.Model):
     __tablename__ = "match_reference"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
 
-    group_id = db.Column(db.String(100), db.ForeignKey("group.id"), nullable=False)
+    group_id = sa.Column(sa.String(100), sa.ForeignKey("group.id"), nullable=False)
 
-    index = db.Column(db.Integer, nullable=False)
+    index = sa.Column(sa.Integer, nullable=False)
 
-    team1_id = db.Column(db.String(100), db.ForeignKey("team.id"), nullable=True)
-    team2_id = db.Column(db.String(100), db.ForeignKey("team.id"), nullable=True)
+    team1_id = sa.Column(sa.String(100), sa.ForeignKey("team.id"), nullable=True)
+    team2_id = sa.Column(sa.String(100), sa.ForeignKey("team.id"), nullable=True)
 
-    bet_type_from_match = db.Column(SqlEnum(BetMapping), nullable=False)
+    bet_type_from_match = sa.Column(SqlEnum(BetMapping), nullable=False)
 
 
 class MatchModel(db.Model):
     __tablename__ = "match"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
 
-    group_id = db.Column(db.String(100), db.ForeignKey("group.id"), nullable=False)
-    group = db.relationship("GroupModel", foreign_keys=group_id, backref="matches")
+    group_id = sa.Column(sa.String(100), sa.ForeignKey("group.id"), nullable=False)
+    group = relationship("GroupModel", foreign_keys=group_id, backref="matches")
 
-    index = db.Column(db.Integer, nullable=False)
+    index = sa.Column(sa.Integer, nullable=False)
 
-    team1_id = db.Column(db.String(100), db.ForeignKey("team.id"), nullable=True)
-    team1 = db.relationship("TeamModel", foreign_keys=team1_id)
+    team1_id = sa.Column(sa.String(100), sa.ForeignKey("team.id"), nullable=True)
+    team1 = relationship("TeamModel", foreign_keys=team1_id)
 
-    team2_id = db.Column(db.String(100), db.ForeignKey("team.id"), nullable=True)
-    team2 = db.relationship("TeamModel", foreign_keys=team2_id)
+    team2_id = sa.Column(sa.String(100), sa.ForeignKey("team.id"), nullable=True)
+    team2 = relationship("TeamModel", foreign_keys=team2_id)
 
-    score_bets = db.relationship("ScoreBetModel", back_populates="match")
-    binary_bets = db.relationship("BinaryBetModel", back_populates="match")
+    score_bets = relationship("ScoreBetModel", back_populates="match")
+    binary_bets = relationship("BinaryBetModel", back_populates="match")
 
 
 class TeamModel(db.Model):
     __tablename__ = "team"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    code = db.Column(db.String(10), unique=True, nullable=False)
-    description = db.Column(db.String(100), unique=True, nullable=False)
-    flag_url = db.Column(db.String(100))
+    code = sa.Column(sa.String(10), unique=True, nullable=False)
+    description = sa.Column(sa.String(100), unique=True, nullable=False)
+    flag_url = sa.Column(sa.String(100))
 
     def to_dict(self) -> dict:
         return {
@@ -354,18 +356,18 @@ class TeamModel(db.Model):
 
 class GroupModel(db.Model):
     __tablename__ = "group"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    code = db.Column(db.String(1), primary_key=True, unique=True, nullable=False)
-    description = db.Column(db.String(100), unique=True, nullable=False)
-    index = db.Column(db.Integer, nullable=False)
+    code = sa.Column(sa.String(1), primary_key=True, unique=True, nullable=False)
+    description = sa.Column(sa.String(100), unique=True, nullable=False)
+    index = sa.Column(sa.Integer, nullable=False)
 
-    phase_id = db.Column(db.String(100), db.ForeignKey("phase.id"), nullable=False)
-    phase = db.relationship("PhaseModel", backref="groups")
+    phase_id = sa.Column(sa.String(100), sa.ForeignKey("phase.id"), nullable=False)
+    phase = relationship("PhaseModel", backref="groups")
 
     def to_dict_with_phase_id(self) -> dict:
         return {
@@ -385,15 +387,15 @@ class GroupModel(db.Model):
 
 class PhaseModel(db.Model):
     __tablename__ = "phase"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    code = db.Column(db.String(10), primary_key=True, unique=True, nullable=False)
-    description = db.Column(db.String(100), nullable=False)
-    index = db.Column(db.Integer, nullable=False)
+    code = sa.Column(sa.String(10), primary_key=True, unique=True, nullable=False)
+    description = sa.Column(sa.String(100), nullable=False)
+    index = sa.Column(sa.Integer, nullable=False)
 
     def to_dict(self) -> dict:
         return {
@@ -405,40 +407,40 @@ class PhaseModel(db.Model):
 
 class GroupPositionModel(db.Model):
     __tablename__ = "group_position"
-    id = db.Column(
-        db.String(100),
+    id = sa.Column(
+        sa.String(100),
         primary_key=True,
         nullable=False,
         default=lambda: str(uuid4()),
     )
-    won = db.Column(db.Integer, CheckConstraint("won>=0"), nullable=False, default=0)
-    drawn = db.Column(db.Integer, CheckConstraint("drawn>=0"), nullable=False, default=0)
-    lost = db.Column(db.Integer, CheckConstraint("lost>=0"), nullable=False, default=0)
-    goals_for = db.Column(db.Integer, CheckConstraint("goals_for>=0"), nullable=False, default=0)
-    goals_against = db.Column(
-        db.Integer,
+    won = sa.Column(sa.Integer, CheckConstraint("won>=0"), nullable=False, default=0)
+    drawn = sa.Column(sa.Integer, CheckConstraint("drawn>=0"), nullable=False, default=0)
+    lost = sa.Column(sa.Integer, CheckConstraint("lost>=0"), nullable=False, default=0)
+    goals_for = sa.Column(sa.Integer, CheckConstraint("goals_for>=0"), nullable=False, default=0)
+    goals_against = sa.Column(
+        sa.Integer,
         CheckConstraint("goals_against>=0"),
         nullable=False,
         default=0,
     )
-    need_recomputation = db.Column(db.Boolean, nullable=False, default=False)
+    need_recomputation = sa.Column(sa.Boolean, nullable=False, default=False)
 
-    user_id = db.Column(
-        db.String(100),
-        db.ForeignKey("user.id", ondelete="CASCADE"),
+    user_id = sa.Column(
+        sa.String(100),
+        sa.ForeignKey("user.id", ondelete="CASCADE"),
         nullable=False,
     )
 
-    team_id = db.Column(
-        db.String(100),
-        db.ForeignKey("team.id"),
+    team_id = sa.Column(
+        sa.String(100),
+        sa.ForeignKey("team.id"),
         nullable=False,
     )
-    team = db.relationship("TeamModel")
+    team = relationship("TeamModel")
 
-    group_id = db.Column(
-        db.String(100),
-        db.ForeignKey("group.id"),
+    group_id = sa.Column(
+        sa.String(100),
+        sa.ForeignKey("group.id"),
         nullable=False,
     )
 
