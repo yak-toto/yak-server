852aa6b43605db34da3abdd7719a59bcdfc5eb32
clarisse <gui.lepape25@gmail.com>
2023-04-26 20:59:15 +0200

    Add strawberry extensions
    new extensions are : MaxAliasesLimiter, MaxTokensLimiter, ValidationCache, ParserCache. Add NoSchemaIntrospectionCustomRule extension in production

--- yak_server/v2/__init__.py
+++ yak_server/v2/__init__.py
@@ -1,11 +1,32 @@
 import strawberry
-from strawberry.extensions import QueryDepthLimiter
+from graphql.validation import NoSchemaIntrospectionCustomRule
+from strawberry.extensions import (
+    AddValidationRules,
+    MaxAliasesLimiter,
+    MaxTokensLimiter,
+    ParserCache,
+    QueryDepthLimiter,
+    ValidationCache,
+)
 
 from .mutation import Mutation
 from .query import Query
 
-schema = strawberry.Schema(
-    query=Query,
-    mutation=Mutation,
-    extensions=[QueryDepthLimiter(max_depth=6)],
-)
+
+def get_schema(debug: bool) -> strawberry.Schema:  # noqa: FBT001
+    extensions = [
+        QueryDepthLimiter(max_depth=6),
+        MaxAliasesLimiter(max_alias_count=30),
+        MaxTokensLimiter(max_token_count=500),
+        ValidationCache(),
+        ParserCache(),
+    ]
+
+    if not debug:
+        extensions.append(AddValidationRules([NoSchemaIntrospectionCustomRule]))
+
+    return strawberry.Schema(
+        query=Query,
+        mutation=Mutation,
+        extensions=extensions,
+    )
