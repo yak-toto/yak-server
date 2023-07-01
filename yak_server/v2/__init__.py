7ce26aa97b1edbaf5b9a5084e5118e4cc6ff73f1
guillaumeLepape <gui.lepape25@gmail.com>
2023-07-01 20:34:06 +0200

    Add should_ignore parameter to QueryDepthLimiter to avoid warning

--- yak_server/v2/__init__.py
+++ yak_server/v2/__init__.py
@@ -2,6 +2,7 @@ import strawberry
 from graphql.validation import NoSchemaIntrospectionCustomRule
 from strawberry.extensions import (
     AddValidationRules,
+    IgnoreContext,
     MaxAliasesLimiter,
     MaxTokensLimiter,
     ParserCache,
@@ -14,8 +15,11 @@ from .query import Query
 
 
 def get_schema(debug: bool) -> strawberry.Schema:
+    def should_ignore(_: IgnoreContext) -> bool:
+        return False
+
     extensions = [
-        QueryDepthLimiter(max_depth=6),
+        QueryDepthLimiter(max_depth=6, should_ignore=should_ignore),
         MaxAliasesLimiter(max_alias_count=30),
         MaxTokensLimiter(max_token_count=500),
         ValidationCache(),
