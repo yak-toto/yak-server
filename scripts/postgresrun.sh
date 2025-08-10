4d1e1f2a1662cc0da9221529da5584b1a93cf355
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-08-10 13:04:54 +0200

    Fix sh formatting precommit hook

--- scripts/postgresrun.sh
+++ scripts/postgresrun.sh
@@ -2,8 +2,8 @@
 source $(dirname $0)/../.env.db
 
 docker run --name yaktoto-postgres \
-	-e POSTGRES_USER=$POSTGRES_USER \
-	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-	-e POSTGRES_DB=$POSTGRES_DB \
-	-p $POSTGRES_PORT:5432 \
-	-d postgres:17.5-alpine3.22
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_DB=$POSTGRES_DB \
+    -p $POSTGRES_PORT:5432 \
+    -d postgres:17.5-alpine3.22
--- scripts/tox.sh
+++ scripts/tox.sh
@@ -1,11 +1,11 @@
 rm -f .coverage
 
 for version in "3.9" "3.10" "3.11" "3.12" "3.13"; do
-	uv run --isolated --all-extras --managed-python -p $version -- \
-		pytest --cov=yak_server --cov=scripts --cov=tests --cov=testing \
-		--cov-config=pyproject.toml \
-		--cov-append \
-		-vv
+    uv run --isolated --all-extras --managed-python -p $version -- \
+        pytest --cov=yak_server --cov=scripts --cov=tests --cov=testing \
+        --cov-config=pyproject.toml \
+        --cov-append \
+        -vv
 done
 
 coverage html
