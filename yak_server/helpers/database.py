c45eee3b764ad19d12c49197728d8046ab31eec3
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-10-12 16:02:50 +0200

    Cache engine building to improve performance drastically

--- yak_server/helpers/database.py
+++ yak_server/helpers/database.py
@@ -1,14 +1,19 @@
 from collections.abc import Generator
+from functools import cache
 
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from yak_server.database import build_engine, build_local_session_maker
 
 
-def get_db() -> Generator[Session, None, None]:
+@cache
+def _get_sqlalchemy_session_maker() -> sessionmaker[Session]:
     engine = build_engine()
+    return build_local_session_maker(engine)
 
-    local_session_maker = build_local_session_maker(engine)
+
+def get_db() -> Generator[Session, None, None]:
+    local_session_maker = _get_sqlalchemy_session_maker()
 
     with local_session_maker() as db:
         yield db
