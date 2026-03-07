2ef3a579581b8e994442ac60851b7ce8cfe80e61
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-03-07 01:26:41 +0100

    Validate settings on docker start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,17 @@ yak db init
 echo "Creating admin user"
 yak db admin --password $ADMIN_PASSWORD
 
+echo "Validating environment variables and settings"
+python -c "
+from yak_server.database.settings import PostgresSettings
+from yak_server.helpers.settings import Settings, AuthenticationSettings, CookieSettings
+
+PostgresSettings()
+Settings()
+AuthenticationSettings()
+CookieSettings()
+"
+
 echo "Container setup done!"
 
 exec "$@"
